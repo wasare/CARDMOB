54a8f5728e166d12a3d51a7dc319620ec315c0d9
Wanderson Reis <wasare@gmail.com>
2025-04-29 08:54:27 -0300

    Exemplo: contador + imagem

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -1,11 +1,34 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
+
+  /// CRUD em memória
   return (
     <View style={styles.container}>
-      <Text>Olá App React Native!</Text>
+      <Text style={styles.text}>Olá App React Native - Atualiza!</Text>
+      <Image 
+        source={{uri: "https://picsum.photos/200"}}
+        style={{width: 200, height: 200}}
+      />
+
       <StatusBar style="auto" />
+      <Text style={styles.text}>Counter: {counter}</Text>
+
+      <View style={styles.buttonContainer}>
+        <Button title='Increment' onPress={incrementCounter} />
+        <Button title='Decrement' onPress={decrementCounter} />
+      </View>
     </View>
   );
 }
@@ -17,4 +40,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  }
 });
