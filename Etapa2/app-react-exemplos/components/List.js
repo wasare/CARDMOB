ee574ea12ca9d5f292df0acbf8bf2575dfc1b3df
Wanderson Reis <wasare@gmail.com>
2025-05-06 10:13:00 -0300

    Add List component [WIP]

---
+++ Etapa2/app-react-exemplos/components/List.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+class List extends Component {
+    state = {
+        names: [
+            {id: 0, name: 'Ben'},
+            {id: 1, name: 'Susan'},
+            {id: 2, name: 'Roberth'},
+            {id: 3, name: 'Chrischarles'}
+        ]
+    }
+    alertItemName = (item) => { 
+        alert(item.name);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text style={styles.text}>
+                    Lista de itens "clicáveis"
+                </Text>
+            </View>
+        );
+    }
+}
+export default List;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        marginTop: 3,
+        backgroundColor: '#d9f9b1',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#4f603c',
+    }
+});
