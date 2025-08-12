cde78941bda94c4571719ebf40bb78497a3fe55a
Wanderson Reis <wasare@gmail.com>
2025-08-12 08:41:12 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/src/screens/HomeScreen.tsx
+++ Etapa3/loja-react-app/src/screens/HomeScreen.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { useTheme } from '../contexts/ThemeContext'; // Nova
 
 function HomeScreen() {
-
+    const { theme, toggleTheme } = useTheme();
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
+            <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
+                Home Screen
+            </Text>
+            <Button title="Alternar Tema" color={theme.colors.primary} onPress={toggleTheme}/>
         </View>
     );
 }
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
