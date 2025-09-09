d6af38b88165c07e19dec4075ea3c40c937a1158
Wanderson Reis <wasare@gmail.com>
2025-09-09 08:44:04 -0300

    Tela de catálogo de produtos

--- Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
+++ Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
@@ -11,6 +11,7 @@ import HomeScreen from "../screens/HomeScreen";
 // importar depois que implementar: DetailsScreen, SettingsScreen
 import RegisterScreen from "../screens/RegisterScreen";
 import LoginScreen from "../screens/LoginScreen";
+import CatalogScreen from "../screens/catalog/CatalogScreen";
 
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
@@ -21,8 +22,8 @@ function TabNavigator() {
             screenOptions={({route, navigation}) => ({
               tabBarIcon: ({ color, focused, size}) => {
                 let iconName;
-                if (route.name === "Home") {
-                  iconName = focused ? "home" : "home";
+                if (route.name === "Catalog") {
+                  iconName = focused ? "tags" : "tags";
                 }
                 return <FontAwesome name={iconName} size={size} color={color} />
               },
@@ -31,7 +32,11 @@ function TabNavigator() {
               headerShown: false,
             })}
           >
-            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen 
+              name="Catalog"
+              component={CatalogScreen}
+              options={{title: 'Menu'}}
+              />
             <Tab.Screen name="Settings" component={HomeScreen} />
             <Tab.Screen name="Register" component={RegisterScreen} />
         </Tab.Navigator>
