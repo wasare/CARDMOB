3ffdd5ed551640c3b316b0fa3308cfb32323c9a9
Wanderson Reis <wasare@gmail.com>
2025-09-23 08:18:11 -0300

    carrinho de compras mínimo

---
+++ Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
@@ -0,0 +1,60 @@
+import React, { useContext } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+const CartItem = ({ item }: any) => {
+    // @todo implementar o context para persistir dados do carrinho.
+
+    const handleRemove = () => {
+        console.log('exclui produto');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <View>
+                <Text style={styles.name}>{item.name}</Text>
+                <View style={styles.quantity}>
+                    <Text style={styles.price}>R$ {(item.price * item.quantity).toFixed(2)}</Text>
+                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
+                        <Text style={styles.buttonText}>-</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.quantityValue}>{item.quantity}</Text>
+
+                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
+                        <Text style={styles.buttonText}>+</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => handleRemove()} style={styles.button}>
+                        <Text style={styles.buttonText}>Remover</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+
+}
+export default CartItem;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+    },
+    image: {
+        width: '50%',
+        height: 100,
+        borderRadius: 8,
+        borderColor: '#ddd',
+    },
+    name: {},
+    quantity: {},
+    price: {},
+    button: {},
+    buttonText: {},
+    quantityValue: {},
+
+});
