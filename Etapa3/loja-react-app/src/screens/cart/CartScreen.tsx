8b7d13c67aaf695ae084fa44a390a7f87242cef7
Wanderson Reis <wasare@gmail.com>
2025-11-04 07:50:04 -0300

    Concluir carrinho de compras

--- Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
@@ -6,7 +6,7 @@ import CartItem from './CartItem';
 import { useShop } from '../../contexts/ShopContext';
 
 const CartScreen = ({ navigation }: any) => {
-    const { cartItems } = useShop();
+    const { cartItems, getTotalPrice, clearCart } = useShop();
 
     const renderItem = ({item} : any) => (
         <CartItem item={item} />
@@ -35,9 +35,9 @@ const CartScreen = ({ navigation }: any) => {
                         keyExtractor={(item: any) => item.id.toString()}
                     />
                     <View style={styles.totalContainer}>
-                        <Text style={styles.totalText}>Total R$ {handleCheckout}</Text>
+                        <Text style={styles.totalText}>Total R$ {getTotalPrice().toString()}</Text>
                         <TouchableOpacity
-                            onPress={handleCheckout}
+                            onPress={clearCart}
                             style={styles.clearButton}
                         >
                             <Text style={styles.clearButtonText}>Limpar carrinho</Text>
